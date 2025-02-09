84f4f6ebf63ff62b2a5c1023bde599986e633a24
SHANTO <rabbymd55555@gmail.com>
2025-02-10 00:37:31 +0600

    added odd.js file

---
+++ for-loop/odd.js
@@ -0,0 +1,22 @@
+// for(let num =1;num<10; num++){
+//     console.log(num);
+// }
+
+
+
+
+// for (let i = 0; i <= 20; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+
+// for(let i =0;i<=20;i=i+2){
+//     console.log(i);
+// }
+
+
+for(let i =1;i<=20;i=i+2){
+    console.log(i);
+}
