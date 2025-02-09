9d1e23930ee6514ce77c4e5d45359f877a9c9981
SHANTO <rabbymd55555@gmail.com>
2025-02-09 23:15:19 +0600

    added Task-2.js file

---
+++ while-loop/Task-2.5.js
@@ -0,0 +1,12 @@
+/***
+Subtask-2:
+Find all the even numbers from 78 to 98.
+ */
+
+// let num = 78;
+// while (num<=98){
+//     if(num%2!==0){
+//         console.log(num);
+//     }
+//     num++
+// }
--- while-loop/Task-2.js
+++ while-loop/Task-2.js
@@ -5,6 +5,8 @@ Find all the odd numbers from 61 to 100.
 
 let num = 61;
 while (num<=100){
-    console.log(num);
+    if(num%2===0){
+        console.log(num);
+    }
     num++
 }
