e4a448699c3389194acb7d497163c8140bcc5389
SHANTO <rabbymd55555@gmail.com>
2025-02-09 18:31:01 +0600

    added problem1.js file

---
+++ for-of-loop/for_of.js
@@ -0,0 +1,5 @@
+const numbers = [20,2,34,56,70];
+
+for (const number of numbers){
+    console.log(number);
+}
---
+++ for-of-loop/problem1.js
@@ -0,0 +1,6 @@
+const fruits  = ['mango','banana','orange','watermelon','strawberry']
+
+for(const fruit of fruits){
+    console.log(fruit);
+    console.log("I want to fruits");
+} 
