c5edfe3d78651c5b41a032edb59a61d170e296fd
SHANTO <rabbymd55555@gmail.com>
2025-02-10 13:58:02 +0600

    added break-2.js file

--- do-while-loop/Break-2.js
+++ do-while-loop/Break-2.js
@@ -1,3 +1,14 @@
 /* 
 Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
 */
+
+let sum = 0;
+let n = 1;
+while(n<=120){
+    sum+=n;
+    console.log(sum);
+    if(sum>=100){
+        break;
+    }
+    n++
+}
