4e35077be0b9c34e025762d09f4bbd9141ff4640
SHANTO <rabbymd55555@gmail.com>
2025-02-10 10:17:29 +0600

    added continue.js file

---
+++ for-loop/continue.js
@@ -0,0 +1,17 @@
+// for(let i=1; i<=10; i++ ){
+//     if(i%2===0){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+// use of while loop
+let n = 0;
+while(n<=10){
+    n++
+    if(n%2===0){
+        continue;
+    }
+    console.log(n);
+}
