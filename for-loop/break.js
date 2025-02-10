e83a06aca57015845c584ff5af005537711ed5fb
SHANTO <rabbymd55555@gmail.com>
2025-02-10 10:04:10 +0600

    added break.js file

---
+++ for-loop/break.js
@@ -0,0 +1,20 @@
+// for(let i=0; i<=50;i++){
+//     console.log(i);
+//     if(i>=5){
+//         break;
+//     }
+// }
+
+
+
+
+// use of while loop
+
+let i = 0;
+while(i<=50){
+    if(i>=10){
+        break;
+    }
+    console.log(i);
+    i++
+}
