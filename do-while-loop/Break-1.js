af87021fba439c070a4da489927a737ed46bb1d5
SHANTO <rabbymd55555@gmail.com>
2025-02-10 11:05:08 +0600

    added Break-1.js file

---
+++ do-while-loop/Break-1.js
@@ -0,0 +1,24 @@
+/* 
+Write a loop 1 to 200. Use break to exit the loop once you find 100.
+*/
+
+// Use for loop>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// for(let i = 1; i<=200; i++){
+//     console.log(i);
+//     if(i===100){
+//         break;
+//     }
+// }
+
+
+
+// Use while loop>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+let i =1;
+while(i<=200){
+    console.log(i);
+    if(i===100){
+        break
+    }
+    i++
+}
