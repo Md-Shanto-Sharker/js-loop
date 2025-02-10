4619a9a681a37cff6040335b18d50afa097bb0be
SHANTO <rabbymd55555@gmail.com>
2025-02-10 10:27:19 +0600

    added do-while.js file

---
+++ do-while-loop/do-while.js
@@ -0,0 +1,31 @@
+// let n = 0;
+// do{
+//     console.log(n);
+//     n++
+// }
+// while(n<10)
+
+
+// let n = 0;
+// do{
+//     n++
+//     if(n%2===0){
+//         continue;
+//     }
+//     console.log(n);
+// }
+// while(n<10);
+
+
+
+
+
+let n = 0;
+do{
+    console.log(n);
+    n++
+    if(n===5){
+        break;
+    }
+}
+while(n<10);
